d926763c46a1b6350550a755359046a50d486b01
Callum <callumpb@hotmail.com>
2017-10-15 22:54:20 +0100

    save methods added to .rb files

--- console.rb
+++ console.rb
@@ -9,16 +9,16 @@ require_relative('models/ticket.rb')
 
 
 customer1 = Customer.new({'name' => 'Kalina', 'funds' => '100'})
-#customer1.save
+customer1.save
 customer2 = Customer.new({'name' => 'Callum', 'funds' => '500'})
 
 film1 = Film.new({'title' => 'Star Wars', 'price' => '10'})
-#film1.save
+film1.save
 film2 = Film.new({'title' => 'Interstellar', 'price' => '15'})
 
 ticket1 = Ticket.new({'customer_id' => customer1.id, 'film_id' => film1.id})
-#ticket1.save
+ticket1.save
 ticket2 = Ticket.new({'customer_id' => customer2.id, 'film_id' => film2.id})
-
+#ticket2.save
 binding.pry
 nil
